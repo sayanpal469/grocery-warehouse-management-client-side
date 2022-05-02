ccaa6a077c75415c26fe50df56b8d2999b424450
sayanpal469 <sayanpal469@gmail.com>
2022-05-02 17:27:33 +0530

    google auth added

--- src/components/LoginArea/SocialLogin/SocialLogin.js
+++ src/components/LoginArea/SocialLogin/SocialLogin.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './SocialLogin.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { AiFillGithub } from "react-icons/ai";
+import auth from '../../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate()
+
+    if(user) {
+        navigate('/')
+    }
     return (
         <div className='social'>
         
-        <Link  className='size me-2' to='/'>
+        <Link onClick={() => signInWithGoogle()} className='size me-2' to='#'>
         <FcGoogle/>
         </Link>
 
-        <Link  className='size text-dark ms-2' to='/'>
+        <Link  className='size text-dark ms-2' to='#'>
         <AiFillGithub/>
         </Link>
     </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,8 +8,8 @@ const Nav = () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top">
   <div className="container">
-    <Link className="navbar-brand text-danger" to="#">
-    Healthy Piorr
+    <Link className="navbar-brand" to="#">
+      <p id='brand'>Healthy Piorr</p>
     </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -17,7 +17,7 @@ const Nav = () => {
     <div className="collapse navbar-collapse" id="navbarText">
       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+          <Link  className="nav-link" aria-current="page" to="/home">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="#">Features</Link>
@@ -32,7 +32,7 @@ const Nav = () => {
         Login/Register
       </Link>
 
-      
+
     </div>
   </div>
       </nav>  
